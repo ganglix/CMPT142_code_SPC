baa3f090edb8adec769fd24f962badd624ceeeb7
Gang <ganglix@gmail.com>
2023-10-10 23:51:01 -0600

    Initial commit

---
+++ week_1/names.py
@@ -0,0 +1,7 @@
+import random
+name_lis = ["Samuel", "Chase", "Uday", "Gang", "James", "Brayden", "Toryn", "Lyndon", "Dima", "Justin"]
+emoji_lis = ["\U0001F600", "\U0001F602", "\U0001F605", "\U0001F609", "\U0001F914", "\U0001F60E"]
+name = random.choice(name_lis)
+emoji = random.choice(emoji_lis)
+
+print(emoji, name)
