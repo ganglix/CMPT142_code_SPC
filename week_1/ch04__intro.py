10e47a03ae582e9ee37a67da74bd7bbb8e70ee8b
Gang <ganglix@gmail.com>
2023-10-21 11:40:21 -0600

    update prep, ch05->04

---
+++ week_1/ch04__intro.py
@@ -0,0 +1,19 @@
+# Indexing and Slicing of !Sequences!
+
+
+
+# •What is indexing used for?
+# •What does a positive index mean?
+# •What does a negative index mean?
+
+
+
+
+# •What is slicing used for?
+# •What are the meanings of the First, second, and third
+# numbers in a slicing operation?
+
+
+
+
+# I want to mention omitted value, index out of range, and range of slice
