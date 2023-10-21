27b25fc72145027eb81bf1c9dd563ca317a5a6ee
Gang <ganglix@gmail.com>
2023-10-21 11:35:31 -0600

    update in class

---
+++ week_1/ch03__intro.py
@@ -0,0 +1,54 @@
+# data types: int, float, bool, str, list, tuple, dict, array
+#
+# int
+100
+
+# string
+'compute'
+"science"
+
+'computer "science"'
+
+# list, collection of data, compound datatype
+[1, 2, "computer"]
+[1, 2, "computer", [1,2]]
+
+
+# boolean value
+True
+False
+
+# above are literals
+
+
+
+# variable
+my_text = "computer"
+print(my_text)
+my_text = "science"
+print(my_text)
+
+# first (out of three) difficult thing:
+# have a good variable name
+# name: descriptive, concise, readable
+# # snake_case
+# my_text_short
+# # camelCase
+# myTextShort
+
+# Ex 1
+# "" computer "" #invalid
+# 007  #invalid
+#007.0  #valid
+print(-7e1) # valid, float, scientific notation
+# print(7e)  # 7e0
+print(7e0)
+
+print( (1 - (-2) ** 4) )
+
+print("a" + "b"*3)
+print("1" + "2")
+print("~" * 20)
+_a = int("123")
+print(_a)
+# print(2abc)# invalid var name
