382fbdbce17ffdef38898c4223a19eb43b24fb3b
Gang <ganglix@gmail.com>
2023-10-28 15:15:45 -0600

    prepare

---
+++ week_3/ch09__intro.py
@@ -0,0 +1,15 @@
+# Control Flow: Repetition
+# what is a computer good at?
+# repeat till what? when?
+
+
+# while loop starts and stops according to a condition
+## things I want to mention: condition boolean, while/if, keep checking(eg.)
+
+# counting example
+
+
+
+# for : repeat over a sequence
+## mention types of sequence: string, range, list, dict
+
---
+++ week_3/ch09_demo2.py
@@ -0,0 +1,16 @@
+# while vs. for
+# In this demonstration, we’ll see a situation where using a
+# while-loop is preferable to using a for-loop.
+
+# password example
+
+# initiate param
+passcode = "cmpt142"
+max_attempts = 3
+
+# ask user to input passcode
+
+# print message to user
+# when while loop is done, there are two outcomes
+
+
