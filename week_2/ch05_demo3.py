b8462558093795e28a809963804d9e1e49f4fde2
Gang <ganglix@gmail.com>
2023-10-28 10:05:47 -0600

    update inclass

--- week_2/ch05_demo3.py
+++ week_2/ch05_demo3.py
@@ -1,27 +1,54 @@
 # This demonstration shows you how to call methods on string objects.
 
-# Let's do this in the interactive mode in console
 
 # initialize some variables to play with
 url = "www.engineering.usask.ca"
 
 # find the location of the first period in the url
+location = url.find(".")
+print(location)
+help(url.find)
+
+# find the location of the second period
+print(   url.find(".", url.find(".") +1, len(url) )  )
+
+# similar to .find(), we have .index()
+print(url.find("z"))  # return -1
+# print(url.index("z"))  # cause an error
+
 
 # count the number of periods in the url
 # (function not covered in readings)
-
+print(url.count("."))
 #
-# remove white space
 
+# remove white space
+s = "  CMPT 142  "
+print(s.rstrip())    # removes trailing white space, \n, \r
+print(s.lstrip())
+print(s.strip())
+
+# what if we want to remove all spaces
+
+print(s.replace(" ", ""))  # replace " " by empty string
 
 # count the number of characters in the url after removing "www."
 # (gotta be careful - don’t just assume a function does what you think!)
-
+print(url.lstrip("www.")) # this removes "garbage chars" w and .
+print(url.lstrip("ww."))  # this removes "garbage chars" w and .
+print(url.lstrip("w."))   # this removes "garbage chars" w and .
+help(url.lstrip)
 
 # # more methods:
+print(url.upper())
+# .lower()
+# url.title()
+
 
 # when are they useful
 # immutable, they create a copy
 
 
 # how do I know what kind of methods are out there? How do I know the syntax/usage?
+
+print(dir(s))
--- week_2/ch05_ex3.py
+++ week_2/ch05_ex3.py
@@ -1,17 +1,21 @@
 # Given variables:
 nation = "Canada !!!"
-nation_motto = "From Sea to Sea"
+nation_motto = "From sea to Sea"
 
 print(
-# Write expressions to:
-# (a) Determine if nation has only alphabetic letters and digits
+    # Write expressions to:
+    # (a) Determine if nation has only alphabetic letters and digits
+    nation.isalnum(),
 
+    # (b) Remove "!" characters and whitespace from nation
+    nation.replace("!", "").replace(" ", ""),
+    nation.replace("!", "").replace(" ", "").isalnum(),
 
+    # (c) Find the location of the first "Sea" substring in nation_motto
+    nation_motto.find("Sea"),
 
-# (b) Remove "!" characters and whitespace from nation
+    # (bonus) find the location of "sea", not case sensitive
+    nation_motto.upper().find("SEA")
 
-
-# (c) Find the location of the first "Sea" substring in nation_motto
-
+    , sep="\n"
 )
-
