b250693de4d396672f8e6643c6633fe14994e0ea
Gang <ganglix@gmail.com>
2023-11-24 13:32:37 -0600

    prepare

---
+++ week_6/ch17__intro.py
@@ -0,0 +1,37 @@
+# Search algorithm
+
+
+# search for a number from a list of numbers
+
+# generate an array to work with
+
+import numpy as np
+# c = np.random.uniform(0,9, size=5).astype(int)
+# print(c)
+
+
+
+
+# linear search
+def linear_membership_search(c, target_key):
+    """
+    a linear membership search for the target key
+
+    c:          a sequence of numbers or strings
+    target_key: the target key for the search
+    return: True if target key is in the collection
+    """
+
+
+
+
+# binary search
+
+def binary_membership_search(c, target_key):
+    """
+    A binary membership search function
+    :param c: list, collection of data, sorted by its search keys
+    :param target_key: target key to search for
+    :return: True if target key is in the collection
+    """
+
